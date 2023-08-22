620513f55dda3cc3dca32dc93cfa9bf7d73fac30
Mark Morley <mmorley0395@gmail.com>
2023-08-22 16:55:38 -0400

    pull orm setup out

---
+++ app/routers/database.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
+
+DB_URI = os.getenv("DB_URI")
+
+SQLALCHEMY_DATABASE_URL = DB_URI
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
