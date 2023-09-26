4b668b8092eefce8a0430a4a20697751ab8a6a42
Mark Morley <mmorley0395@gmail.com>
2023-09-26 18:28:23 -0400

    create segment working very excited woooooo

--- app/routers/analyze.py
+++ app/routers/analyze.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from dotenv import load_dotenv
 from data_structures import schemas
+from lts_island_connectivity import StudySegment
 
 load_dotenv()
 
@@ -9,5 +10,13 @@ router = APIRouter()
 
 @router.post("/analyze/")
 def analyze_segment(data: schemas.AnalyzeRequest):
-    print(data)
+    if data.connection_type == 'bike':
+        cx_type = "lts"
+    elif data.connection_type == "pedestrian":
+        cx_type = "sidewalk"
+    else:
+        raise ValueError("connection type must be bike or pedestrian")
+    for feature in data.geo_json.features:
+        StudySegment(cx_type, feature, data.username)
+
     return {"message": "Data received"}
