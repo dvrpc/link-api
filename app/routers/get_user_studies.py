bd6f672bf5aca761b258d3b3a6198ca596b4f2bb
Mark Morley <mmorley0395@gmail.com>
2023-11-20 13:48:02 -0500

    study exists added

--- app/routers/get_user_studies.py
+++ app/routers/get_user_studies.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, Query
 from dotenv import load_dotenv
 from data_structures import schemas, database, crud
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 from fastapi.responses import JSONResponse
 
 load_dotenv()
@@ -9,7 +10,17 @@ load_dotenv()
 router = APIRouter()
 
 
-@router.get("/get_user_studies/", response_model=schemas.UserStudies)
+@router.get("/study_exists/")
+async def study_exists_endpoint(name: str, username: str, schema: str):
+    db: Session = database.get_db_for_schema(schema)
+    query = text("SELECT EXISTS (SELECT 1 FROM {}.user_segments WHERE seg_name = :name AND username = :username)".format(
+        schema))
+
+    result = db.execute(query, {'name': name, 'username': username}).scalar()
+    return {"exists": result}
+
+
+@ router.get("/get_user_studies/", response_model=schemas.UserStudies)
 def user_studies(
         username: str,
         schema: str = Query(...,
