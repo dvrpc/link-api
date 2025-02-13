4b15c9f4711e61bcf194174cccce785eb6531071
= <=>
2025-02-13 16:10:19 -0500

    Added isochrone flag and incomplete segment delete function

--- app/routers/analyze.py
+++ app/routers/analyze.py
@@ -5,12 +5,15 @@ from requests.exceptions import JSONDecodeError
 import time
 from requests.exceptions import ConnectionError
 import psycopg2
+import re
 
 from fastapi import APIRouter, Depends, HTTPException, Query
 from dotenv import load_dotenv
 from lts_island_connectivity import StudySegment, SegmentNameConflictError
+from geoalchemy2 import WKTElement
+from sqlalchemy.orm import Session
 
-from data_structures import schemas
+from data_structures import schemas, database, crud
 from . import basic_auth
 
 load_dotenv()
@@ -21,12 +24,38 @@ pg_config_filepath = os.path.join(
 
 router = APIRouter()
 
+# If isochrone flag is hit, delete the incomplete study that was made
+def delete_incomplete_study(username, schema, seg_name):
+    DB_URI = os.environ.get("DB_URI", "")
+
+    try:
+        conn = psycopg2.connect(DB_URI)
+    except:
+        print("Unable to connect to database")
+    
+    rows_deleted  = 0
+
+    with conn.cursor() as curs:
+        try:
+            query = f"""
+                DELETE FROM {schema}.user_segments
+                WHERE username = '{username}' AND seg_name = '{seg_name}'
+            """
+            curs.execute(query)
+            rows_deleted = curs.rowcount;
+            conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            print(f"Deleted {rows_deleted} incomplete study")
+
 
 @router.post(f"{URL_ROOT}/analyze", response_model=schemas.AnalyzeResponse)
 async def analyze_segment(
     basic_auth: Annotated[str, Depends(basic_auth)],
     data: schemas.AnalyzeRequest,
     overwrite: bool = Query(False, description="Flag to overwrite existing segment"),
+    resubmit: bool = Query(False, description="Flag to override isochrone flag, available on resubmit")
 ):
     if data.connection_type == "bike":
         cx_type = "lts"
@@ -49,6 +78,7 @@ async def analyze_segment(
                     data.username,
                     overwrite=overwrite,
                     pg_config_filepath=pg_config_filepath,
+                    override_isochrone_flag=resubmit
                 )
                 break
             except SegmentNameConflictError as exc:
@@ -72,5 +102,14 @@ async def analyze_segment(
                 time.sleep(initial_wait_time * (2**attempt))
                 overwrite = True
                 attempt += 1
+            except ValueError as exc:
+                exc_str = str(exc)
+                if "Isochrone flag error" in exc_str:
+                    seg_name = exc_str.split('@')[1]
+                    # db = database.get_db_for_schema(cx_type)
+                    delete_incomplete_study(data.username, cx_type, seg_name)
+                    raise HTTPException(status_code=422, detail="Mileage of connected islands exceeds 300")
+                else:
+                    raise HTTPException(status_code=422, detail=exc_str)
 
     return {"message": "Segments processed successfully"}
